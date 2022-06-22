172a5b4b537fee616b82cadd5ec02eeca3d56320
deltakosmos <francescbages@gmail.com>
2022-06-22 20:15:40 +0200

    add put router User

--- server.js
+++ server.js
@@ -1,66 +1,80 @@
-const express= require('express');
-const mongoose= require('mongoose');
-const bodyParser= require('body-parser');
-const port=8000;
-const app= express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 8000;
+const app = express();
 const User = require('./models/User');
 mongoose.connect('mongodb://localhost/userData');
 
 app.use(bodyParser.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
+app.listen(port, () => {
+  console.log(`server is listening on port:${port}`)
 })
 
 // CREATE
-app.post('/users',(req,res)=>{
+app.post('/users', (req, res) => {
   User.create(
     {
       name: req.body.newData.name,
-      email : req.body.newData.email,
+      email: req.body.newData.email,
       password: req.body.newData.password
     },
-    (err,data)=>{
-      if(err){
-        res.json({success: false, message: err})
-      } else if (!data){
-        res.json({success: false, message: "Not found"})
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err })
+      } else if (!data) {
+        res.json({ success: false, message: "Not found" })
       } else {
-        res.json({success: true, message : data})
+        res.json({ success: true, message: data })
       }
     }
   )
 })
 
 app.route('/users/:id')
-// READ
-.get((req,res)=>{
   // READ
-  User.findById(req.params.id,(err,data)=>{
-    if (err){
-      res.json({
-        success: false,
-        message: err
-      })
-    } else if (!data){
-      res.json({
-        success: false,
-        message: "Not Found"
-      })
-    } else {
-      res.json({
-        success: true,
-        data: data
-      })
-    }
+  .get((req, res) => {
+    // READ
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    })
   })
-})
 
-// UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
-})
-// DELETE
-.delete((req,res)=>{
-  // User.findByIdAndDelete()
-})
+  // UPDATE
+  .put((req, res) => {
+    User.findByIdAndUpdate(req.params.id,
+      {
+        name: req.body.newData.name,
+        email: req.body.newData.email,
+        password: req.body.newData.password
+      },
+      (err, data) => {
+        if (err) {
+          res.json({ success: false, message: err })
+        } else if (!data) {
+          res.json({ success: false, message: "Id not found" })
+        } else {
+          res.json({success: true, message: data})
+        }
+      })
+  })
+  // DELETE
+  .delete((req, res) => {
+    // User.findByIdAndDelete()
+  })
